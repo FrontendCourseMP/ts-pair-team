079d0e411e559dd9ad5b4861c820383a147b65a3
Your Name <you@example.com>
2025-11-12 13:40:59 +0300

    Жирова

--- src/task2.ts
+++ src/task2.ts
@@ -4,3 +4,22 @@ class CheckValid {
         ['[', ']'],
         ['{', '}']
     ])};
+
+public isValid(s: string): boolean {
+    const stack: string[] = [];
+    for (const char of s) {
+        if (this.isOpenBracket(char)) {
+            stack.push(char);
+        }
+        else if (this.isCloseBracket(char)) {
+            if (stack.length === 0) {
+                return false;
+            }
+            const lastOpenBracket = stack.pop()!;
+            if (!this.bracketsMatch(lastOpenBracket, char)) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
